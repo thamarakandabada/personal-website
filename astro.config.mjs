6c29a109c8958b3c615c54e10ed1a1465918c5d9
Thamara Kandabada <thamara.flinc@gmail.com>
2026-06-05 15:02:10 +0100

    Remove hybrid output option from Astro config
    Removed hybrid output option to simplify configuration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  output: 'hybrid', // Allows static by default, but opts-in to SSR when needed
   adapter: vercel(),
 });
