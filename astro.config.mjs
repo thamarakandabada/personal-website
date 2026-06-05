b8d523863115d67697ddf0f367d504685b1dc8b2
Thamara Kandabada <thamara.flinc@gmail.com>
2026-06-05 14:21:04 +0100

    Configure Astro with Vercel adapter and hybrid output
    Added Vercel adapter and configured hybrid output.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless'; // Example adapter
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'hybrid', // Allows static by default, but opts-in to SSR when needed
+  adapter: vercel(),
+});
