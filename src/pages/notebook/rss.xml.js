d712706d87796bf8e7c15ffec8051e319c19b91c
Thamara Kandabada <thamara.flinc@gmail.com>
2026-07-18 09:09:57 +0000

    build: add rss feed to notebook, add feeds page

--- src/pages/notebook/rss.xml.js
+++ src/pages/notebook/rss.xml.js
@@ -1,18 +1,25 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt();
 
 export async function GET(context) {
   const posts = await getCollection("blog");
   return rss({
+    stylesheet: '/rss/styles.xsl',
     title: 'Thamara\'s Notebook',
     description: 'Uncensored thoughts on Life, The Universe, and Everything',
     site: context.site,
     items: posts.map((post) => ({
       title: post.data.title,
       pubDate: post.data.pubDate,
+      author: post.data.author,
       description: post.data.description,
       link: `/notebook/${post.id}/`,
-    })),
+      content: sanitizeHtml(parser.render(post.body), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+      }),
     customData: `<language>en-gb</language>`,
   });
 }
